277e70be3291a7824e1ea5857e23c0510a3363cf
erika.takacs <erika.takacs@SWS00670>
2017-03-13 15:35:36 +0100

    paros pozitiv negativ

--- Elso/masodik/Program.cs
+++ Elso/masodik/Program.cs
@@ -10,6 +10,55 @@ namespace masodik
     {
         static void Main(string[] args)
         {
+            int a;
+            Console.Write("Adj meg egy szamot: ");
+            string v =Console.ReadLine();
+            a = Convert.ToInt32(v);
+            if (a < 0){
+                Console.WriteLine("A megadott szam negativ.");
+            }
+            else if (a > 0){
+                Console.WriteLine("A megadott szam pozitiv.");
+
+            }
+            else {
+                Console.WriteLine("A megadott szam 0.");
+            }
+            Console.Write("Adj meg egy szamot: ");
+            v = Console.ReadLine();
+            a = Convert.ToInt32(v);
+            if (a %2 == 0)
+            {
+                Console.WriteLine("A megadott szam paros");
+            }
+            else
+            {
+                Console.WriteLine("A megadott szam paratlan");
+            }
+
+            Console.Write("Adj meg egy szamot: ");
+            v = Console.ReadLine();
+            a = Convert.ToInt32(v);
+            if (a <0 && a%2==0){
+                Console.WriteLine("A megadott szam Negativ páros szám");
+            }
+            else if(a < 0 && a % 2 != 0)
+            {
+                Console.WriteLine("A megadott szam Negativ páratlan szám");
+            }
+            else if( a >0 && a%2==0)
+            {
+                Console.WriteLine("A megadott szam Pozitiv páros szám");
+            }
+            else if (a > 0 && a % 2 != 0)
+            {
+                Console.WriteLine("A megadott szam Pozitiv páratlan szám");
+            }
+            else
+            {
+                Console.WriteLine("Nullat adtal meg");
+            }
+            Console.ReadKey();
         }
     }
 }
