41479887d7016420d6cfbe6f35cd04a98db6d57f
erika.takacs <erika.takacs@SWS00670>
2017-03-14 15:55:00 +0100

    keddi testtomeg

---
+++ feladat orgonasip/feladat orgonasip/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace feladat_orgonasip
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int n = 10;
+            int i;
+            for (i = 0; i < n - 1; i++)
+            {
+                Console.Write("add meg a " +(i+1)+ ".szamot: ");
+                string v = Console.ReadLine();
+                int vekt = new int[];
+                Console.WriteLine(a);
+
+                Console.WriteLine(" ");
+            }
+            Console.ReadKey();
+
+        }
+        }
+}
---
+++ feladat orgonasip/testtomeg index/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testtomeg_index
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Add meg a súlyodat kilogramban: " );
+            string v1 = Console.ReadLine();
+            int suly = Convert.ToInt32(v1);
+            Console.Write("Add meg a magasságodat centiméterben: ");
+            string v2 = Console.ReadLine();
+            double magassag = Convert.ToDouble(v2);
+            double bmi = suly / (magassag * magassag);
+            if (bmi < 18)
+            {
+                Console.WriteLine("Annyira sovány vagy, hogy fáj.");
+            }
+            else if (bmi>=18 || bmi < 25)
+            {
+                Console.WriteLine("Ideális a súlyod csak így tovább");
+            }
+            else if (bmi >=25  || bmi < 30)
+            {
+                Console.WriteLine("Barátom, ideje behúzni a gatya madzagot, kezdesz elhizni");
+            }
+
+            else
+            {
+                Console.WriteLine("Tessék sportolni, elhíztál");
+            }
+            Console.ReadKey();
+        }
+    }
+}
